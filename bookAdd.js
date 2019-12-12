16571bf5135a8f3887e020cee5f3318f03c1e2f7
Matt Werth <55985843+werth-code@users.noreply.github.com>
2019-12-12 09:17:50 -0500

    Add files via upload

---
+++ bookAdd.js
@@ -0,0 +1,31 @@
+/* 
+This is a simple program to add books to an array. 
+It will not add any books that contain the word "great"
+*/
+
+var favoriteBooks = [];
+function addFavoriteBook(bookName) {
+  if(!bookName.includes("Great")) {
+  favoriteBooks.push(bookName);
+  }
+}
+
+function printFavoriteBooks () {
+  console.log(
+    `Favorite Books: ${favoriteBooks.length}`
+  );
+  
+  for (let bookName of favoriteBooks) {
+    console.log(bookName);
+  }
+}
+
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+printFavoriteBooks();
+
